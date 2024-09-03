6b92e5875926032e8bf147988799d80ce7d19f1e
SadMearise <nikita.khrustalev2012@gmail.com>
2024-09-03 21:23:03 +0300

    chore: update eslint rules

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
 const nunito = Nunito({
-  subsets: ['cyrillic'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
